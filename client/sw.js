76ef75bc13d2b832e95a26614f7fc788c6c06e3a
dyphen12 <alexiswong10@gmail.com>
2021-07-24 10:01:06 -0400

    working

--- client/sw.js
+++ client/sw.js
@@ -4,6 +4,6 @@
    const data = event.data.json();
 
    self.registration.showNotification(data.title, {
-     body: 'Yay it works!',
+     body: data.body
    });
  });
--- index.js
+++ index.js
@@ -14,22 +14,50 @@ app.use(express.static(path.join(__dirname, 'client')));
 const publicVapidKey = process.env.PUBLIC_VAPID_KEY;
 const privateVapidKey = process.env.PRIVATE_VAPID_KEY;
 
-webPush.setVapidDetails('mailto:test@example.com', publicVapidKey, privateVapidKey);
+webPush.setVapidDetails('mailto:contact@prismasys.site', publicVapidKey, privateVapidKey);
 
 // Subscribe route
 
 app.post('/subscribe', (req, res) => {
   const subscription = req.body
 
+  console.log(subscription);
+
   res.status(201).json({});
 
   // create payload
   const payload = JSON.stringify({
-    title: 'Push notifications with Service Workers',
+    title: 'Prisma Bounty Hunter',
+    body: 'I will pay you nothing but hey, you can eat!'
   });
 
   webPush.sendNotification(subscription, payload)
     .catch(error => console.error(error));
+});
+
+app.post('/subscribedev', (req, res) => {
+
+    console.log(req.body);
+    console.log('Im here');
+
+    var title = req.body['pushit']['title'];
+    var subtitle = req.body['pushit']['subtitle'];
+    var sub = req.body['pushit']['sub'];
+    var joburl = req.body['pushit']['url'];
+    console.log(joburl);
+
+    res.status(201).json({});
+
+  // create payload
+  const payload = JSON.stringify({
+    title: title,
+    body: subtitle,
+    url: joburl
+  });
+
+  webPush.sendNotification(sub, payload)
+    .catch(error => console.error(error));
+
 });
 
 app.set('port', process.env.PORT || 5000);
